deb91d2195442109e1b372a702d01d59127b320c
DESKTOP-F66M8A9\sfy25 <sfy25700000@gmail.com>
2023-11-28 11:43:00 +0800

    更改加入相同商品到購物車為數量加1

--- MVC product/productModel.php
+++ MVC product/productModel.php
@@ -16,13 +16,26 @@ function getProductList() {
 }
 
 function addToShoppingCart($id) {
-	global $db;
+ global $db;
 
-	$sql = "INSERT INTO shopping_cart (product_id,quantity) VALUES (?,1);"; //SQL中的 ? 代表未來要用變數綁定進去的地方
-	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
-	mysqli_stmt_bind_param($stmt, "i", $id); //bind parameters with variables, with types "sss":string, string ,string
-	mysqli_stmt_execute($stmt);  //執行SQL
-	return True;
+ $sql = "select * from shopping_cart where product_id=?;";
+ $stmt = mysqli_prepare($db, $sql ); //precompile sql指令，建立statement 物件，以便執行SQL
+    mysqli_stmt_bind_param($stmt, "i", $id); 
+ mysqli_stmt_execute($stmt); //執行SQL
+ $result = mysqli_stmt_get_result($stmt); //取得查詢結果
+ //如果查詢有資料
+ if (mysqli_num_rows($result)>0)
+ {
+    $sql = "Update shopping_cart set quantity=quantity+1 where product_id=?;"; //SQL中的 ? 代表未來要用變數綁定進去的地方
+ }
+ else
+ {
+      $sql = "INSERT INTO shopping_cart (product_id,quantity) VALUES (?,1);"; //SQL中的 ? 代表未來要用變數綁定進去的地方
+ }
+ $stmt = mysqli_prepare($db, $sql); //prepare sql statement
+ mysqli_stmt_bind_param($stmt, "i", $id); //bind parameters with variables, with types "sss":string, string ,string
+ mysqli_stmt_execute($stmt);  //執行SQL
+ return True;
 }
 
 
